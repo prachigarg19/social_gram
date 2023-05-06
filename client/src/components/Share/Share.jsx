869a276857db4a27c88e877c085a09491427319d
prachigarg19 <72646230+prachigarg19@users.noreply.github.com>
2023-05-06 16:59:22 +0530

    create post functionality added

--- client/src/components/Share/Share.jsx
+++ client/src/components/Share/Share.jsx
@@ -1,28 +1,83 @@
 import "./share.css";
-import React from "react";
+import React, { useContext, useRef, useState } from "react";
 import PermMediaIcon from "@mui/icons-material/PermMedia";
 import LabelIcon from "@mui/icons-material/Label";
 import RoomIcon from "@mui/icons-material/Room";
 import EmojiEmotionsIcon from "@mui/icons-material/EmojiEmotions";
+import { AuthContext } from "../../contexts/AuthContext";
 
 const Share = () => {
+  const { user } = useContext(AuthContext);
+  const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+  const descRef = useRef();
+  const [file, setFile] = useState(null);
+
+  const clearFields = () => {
+    setFile(null);
+    descRef.current.value = "";
+  };
+
+  const handlePostSubmit = (e) => {
+    e.preventDefault();
+    console.log(file);
+    if (!file) return;
+    const data = { userId: user._id, desc: descRef.current.value || "" };
+
+    fetch("http://localhost:8800/api/posts", {
+      method: "POST", // or 'PUT'
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Success:", data);
+        clearFields();
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
+
   return (
     <div className="share">
       <div className="shareWrapper">
         <div className="shareTop">
-          <img className="shareProfileImg" src="/assets/person/1.jpg" alt="" />
+          <img
+            className="shareProfileImg"
+            src={
+              user.profilePicture
+                ? PF + user.profilePicture
+                : PF + "person/noAvatar.png"
+            }
+            alt=""
+          />
           <input
-            placeholder="What's in your mind Safak?"
+            placeholder={"What's in your mind " + user.username + "?"}
             className="shareInput"
+            ref={descRef}
           />
         </div>
+        {file && (
+          <div className="postCenter">
+            <img className="postImg" src={PF + file.name} alt="" />
+          </div>
+        )}
         <hr className="shareHr" />
-        <div className="shareBottom">
+        <form className="shareBottom" onSubmit={(e) => handlePostSubmit(e)}>
           <div className="shareOptions">
-            <div className="shareOption">
+            <label htmlFor="file" className="shareOption">
               <PermMediaIcon htmlColor="tomato" className="shareIcon" />
               <span className="shareOptionText">Photo or Video</span>
-            </div>
+              <input
+                style={{ display: "none" }}
+                type="file"
+                id="file"
+                accept=".png .jpg .jpeg"
+                onChange={(e) => setFile(e.target.files[0])}
+              />
+            </label>
             <div className="shareOption">
               <LabelIcon htmlColor="blue" className="shareIcon" />
               <span className="shareOptionText">Tag</span>
@@ -36,8 +91,19 @@ const Share = () => {
               <span className="shareOptionText">Feelings</span>
             </div>
           </div>
-          <button className="shareButton">Share</button>
-        </div>
+          <div>
+            <button
+              className={`shareButton${file ? "" : " disabledShareButton"}`}
+              type="submit"
+              disabled={!file}
+            >
+              Share
+            </button>
+            <button className="shareButton" onClick={clearFields}>
+              Cancel
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   );
