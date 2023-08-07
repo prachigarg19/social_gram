8cfaae5ebfcb5641fffaf5df9885c628d8850b77
prachigarg19 <72646230+prachigarg19@users.noreply.github.com>
2023-08-07 21:23:40 +0530

    prevent react strict mode (temp)

--- client/src/index.js
+++ client/src/index.js
@@ -6,11 +6,9 @@ import { LayoutContextProvider } from "./contexts/LayoutContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <AuthContextProvider>
-      <LayoutContextProvider>
-        <App />
-      </LayoutContextProvider>
-    </AuthContextProvider>
-  </React.StrictMode>
+  <AuthContextProvider>
+    <LayoutContextProvider>
+      <App />
+    </LayoutContextProvider>
+  </AuthContextProvider>
 );
